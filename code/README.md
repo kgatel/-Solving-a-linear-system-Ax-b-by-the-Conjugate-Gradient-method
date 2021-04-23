8ec420e11342d46a2fa6259c1c9d9886e5c3bb90
Thibaut <thibaut.andregallis@insa-rouen.fr>
2021-04-23 23:19:47 +0200

    ajout conversion binaire decimale

---
+++ code/README.md
@@ -0,0 +1,6 @@
+tu compiles conversion_binaire_decimale.f90 avec gfortran en appelant conv l'executable par exemple
+tu copies colle  le nombre binaire dans dconv
+tu fais ./conv <dconv >outconv
+et t'auras les résultats dans outconv il reste plus qu'a à calculer A et B dans outconv.
+Tu verras à quoi correspondent A et B dans outconv c'est la valeur exacte de la mantisse et du nombre.
+Je suis désolé y'a plein d'espaces en trop mais j'ai rien réussi à faire de mieux pour les enlever :'( 
--- code/conversion_binaire_decimale_DP.f90
+++ code/conversion_binaire_decimale_DP.f90
@@ -4,11 +4,15 @@ program conversion
 	integer, dimension(11) :: exposant
 	integer, dimension(52) :: mantisse
 	integer signe,exposantdec
-	character*400 :: c1,c2
+	character*64 :: c1
 	logical :: denormalise,positif
 	integer :: i
 	
-	read(*,*) nombre
+	read(*,*) c1
+	do i=1,64
+		read(c1(i:i),'(i1)') nombre(i)
+	enddo
+	
 	signe=nombre(1)
 	
 	if (nombre(1)==0) then
@@ -25,16 +29,20 @@ program conversion
 		mantisse(i-12)=nombre(i)
 	enddo
 	
-	print*, 'Nombre en écriture binaire :\n'
-	write(*,*) nombre
-	print*, '\n'
-	print*, 'bit de signe : '
-	write (*,*) signe
-	print*, 'bits d''exposants : '
-	write(*,*) exposant
-	print*, 'bits de mantisses : '
-	write(*,*) mantisse
-	print*, ('\n')
+	print*, 'Nombre en écriture binaire : ',c1
+	print*, ('')
+	write (c1,'(i1)') signe
+	write (*,*) "bit de signe : ",c1
+	c1=""
+	do i=1,11
+		write(c1(i:i),'(i1)') exposant(i)
+	enddo
+	write(*,*) "bits d''exposants : ",c1
+	do i=1,52
+		write(c1(i:i),'(i1)') mantisse(i)
+	enddo
+	write(*,*) "bits de mantisses : ",c1
+	print*, ('')
 	denormalise=.false.
 	do i=1,11
 		if (exposant(i)/=0) then
@@ -43,46 +51,46 @@ program conversion
 	enddo
 	
 	if (positif) then
-		c1 = "positif"
+		if (denormalise) then
+			print*, 'Le nombre est positif car le bit de signe est à 0 ; dénormalisé car les bits d''exposants sont tous égaux à 0'
+		else
+			print*, 'Le nombre est positif car le bit de signe est à 0 ; normalisé car les bits d''exposants sont différents de : 000...'
+		endif
 	else
-		c1 = "negatif"
+		if (denormalise) then
+			print*, 'Le nombre est négatif car le bit de signe est à 1 ; dénormalisé car les bits d''exposants sont tous égaux à 0'
+		else
+			print*, 'Le nombre est négatif car le bit de signe est à 1 ; normalisé car les bits d''exposants sont différents de : 000...'
+		endif
 	endif
 	
-	if (denormalise) then
-		c2 = "dénormalisé"
-	else
-		c2 = "normalisé"
-	endif
-	
-	if (denormalise) then
-		print*, 'Le nombre est ',c1,' car le bit de signe est à ',signe,' ; ',c2,' car les bits d''exposants sont tous égaux à 0\n'
-	else
-		print*, 'Le nombre est ',c1,' car le bit de signe est à ',signe,' ; ',c2
-		print*, ' car les bits d''exposants sont différents de : 00000000000\n'
-	endif
-	
-	print*, '\n'
+	print*, ''
 	c1=""
 	exposantdec=0.d0
+	print*, 'exposant biaisé = '
 	do i=1,11
-		if (exposant(12-i)==1) then
-			
-			c1= c1//"2^("//11-i//") + "
+		if (exposant(i)==1) then
+			print*, '2^(',11-i,') + '
 			exposantdec=exposantdec+2**(11-i)
 		endif
 	enddo
-	print*, 'exposant biaisé = ',c1,'= ',exposantdec,'\n'
-	print*, 'exposant réel = ',exposantdec,'-d = ',exposantdec,'-1023 = ',(exposantdec-1023),'\n'
-	c2=""
+	
+	print*, '= ',exposantdec
+	print*, ''
+	print*, 'exposant réel = ',exposantdec,'-d = ',exposantdec,'-1023 = ',(exposantdec-1023)
+	exposantdec=exposantdec-1023
+	print*, ''
 	mantissedec=0.d0
+	print*, 'mantisse = '
 	do i=1,52
 		if (mantisse(i)==1) then
-			c2=c2+"2^("+i+") + "
-			mantissedec=mantissedec+2**(-i)
+			print*, '2^(-',i,') + '
 		endif
 	enddo
-	print*, 'mantisse = ',c2,' = A (à calculer sur https://www.dcode.fr/calcul-sommation par exemple)\n\n'
-	print*, 'Valeur décimale exacte : ((-1)^',signe,')*(A)*(2^(',exposantdec,')) = B (à calculer avec wolfraalpha par exemple)'
+	
+	print*,' =  A (à calculer sur https://www.dcode.fr/calcul-sommation par exemple)'
+	print*, ''
+	print*, 'Valeur décimale exacte : ((-1)^',signe,')*(A)*(2^(',exposantdec,')) = B (à calculer avec wolfram alpha par exemple)'
 	
 end program conversion
 	
